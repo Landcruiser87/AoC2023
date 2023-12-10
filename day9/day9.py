db217a9083c5784d5ca4e33bf539af1fc6a4aab2
Landcruiser87 <andyheroy@gmail.com>
2023-12-10 14:26:18 -0600

    finish day 9

--- day9/day9.py
+++ day9/day9.py
@@ -5,6 +5,7 @@ sys.path.append(root_folder)
 from utils.time_run import log_time
 from utils.loc import recurse_dir
 import numpy as np
+from collections import deque
 
 DAY = './day9/'
 def data_load(filen:str)->list:
@@ -13,35 +14,45 @@ def data_load(filen:str)->list:
 		arr = [x.strip() if x != "" else "" for x in data]
 	return arr
 
-def extrapolate(data:list)->list:
-	predictions, tips, depth = [], [], 0
+def extrapolate(data:list, part:str)->list:
+	predictions, tips = [], []
 	for line in data:
 		temp = [int(num) for num in line.split()]
 		while True:
 			diff = np.diff(temp)
-			depth += 1
 			if np.all(temp==0) and len(temp) > 1:
 				break
 			else:
-				tips.append(temp[-1])
+				if part == "A":
+					tips.append(temp[-1])
+				else:
+					tips.append(temp[0])
 				temp = diff
-		predictions.append(sum(tips))
-		tips, depth = [], 0
-		
+		if part == "A":
+			predictions.append(sum(tips))
+		elif part == "B":
+			thecount = 0
+			for tip in tips[::-1]:
+				thecount = tip - thecount			
+			predictions.append(thecount)
+		tips = []
+			
 	return predictions
 
 @log_time
 def part_A():
 	data = data_load("data")
-	predictions = extrapolate(data)
+	predictions = extrapolate(data, "A")
 	return sum(predictions)
 
 @log_time
 def part_B():
-	data = data_load()
+	data = data_load("data")
+	predictions = extrapolate(data, "B")
+	return sum(predictions)
 	
 print(f"Part A solution: \n{part_A()}\n")
-# print(f"Part B solution: \n{part_B()}\n")
+print(f"Part B solution: \n{part_B()}\n")
 print(f"Lines of code \n{recurse_dir(DAY)}")
 
 ########################################################
@@ -51,3 +62,10 @@ print(f"Lines of code \n{recurse_dir(DAY)}")
 #until all values == 0.  At that point, we want to predict the next value
 #in the input sequence.  We do so by ensureing the diffs above it all equal
 #zero  i think ... i can just reverse that process
+
+#So for part A I noticed if you summed up all the ends that would equal
+# your evetual number.  So instead of tracking the whole stack, i just tracked the 
+#ends in its own list.  
+
+#Similarly for part B, If i keep only the first value of each line.  I can then subtract that
+#value from itself all the way back up the chain in reverse to get its value.  Fast and fun! 
