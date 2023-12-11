3473b2fc0c630c0aecee07bc91c188db33bcae48
Landcruiser87 <andyheroy@gmail.com>
2023-12-11 15:47:38 -0600

    still lost

--- day10/day10.py
+++ day10/day10.py
@@ -7,6 +7,28 @@ from utils.loc import recurse_dir
 from itertools import chain
 
 DAY = './day10/'
+MOV_DICT = {
+	"|":["N","S"],
+	"-":["E","W"],
+	"L":["N","E"],
+	"J":["N","W"],
+	"7":["S","W"],
+	"F":["S","E"], 
+	"S":["N","S","E","W"]
+}
+DIRS_DICT = {
+	"N":(-1, 0),
+	"S":(1, 0),
+	"E":(0, 1),
+	"W":(0, -1)
+}
+CON_DICT = {
+	"N":"S",
+	"S":"N",
+	"E":"W",
+	"W":"E"
+}
+
 def data_load(filen:str)->list:
 	with open(f'{DAY}{filen}.txt', 'r') as f:
 		data = f.read().splitlines()
@@ -20,51 +42,69 @@ def onboard(data:list, x1:int, y1:int)->bool:
 		return False
 	else:
 		return True
-	
-MOV_DICT = {
-	"|":["N","S"],
-	"-":["E","W"],
-	"L":["N","E"],
-	"J":["N","W"],
-	"7":["S","W"],
-	"F":["S","E"], 
-	"S":["N","S","E","W"]
-}
-DIRS_DICT = {
-	"N":(-1, 0),
-	"S":(1, 0),
-	"E":(1, 0),
-	"W":(-1, 0)
-}
-def scan_neighbors(data:list, curr_pos:tuple):
-	for direction, (x, y) in DIRS_DICT.items():
-		next_pos = (curr_pos[0] + x, curr_pos[1] + y)
-		if onboard(data, next_pos[0], next_pos[1]) and data[next_pos[0]][next_pos[1]] != ".":
-			next_val = MOV_DICT[data[next_pos[0]][next_pos[1]]]
-			curr_dir = MOV_DICT[data[curr_pos[0]][curr_pos[1]]]
-			if set(curr_dir) & set(next_val) == set(next_val):
-				return next_pos
-				
+
+def scan_neighbors(data:list, curr_p:tuple, next_p:tuple, direction:str):
+	if onboard(data, next_p[0], next_p[1]) and data[next_p[0]][next_p[1]] != ".":
+		next_val = MOV_DICT[data[next_p[0]][next_p[1]]]
+		if len(set(CON_DICT[direction]) & set(next_val)) > 0:
+			#Might need to check different combinations here. 
+			return next_p
+		#TODO - Need to rewrite this section to accomodate S starts
+
 def follow_the_paths(data:list)->int:
 	start = [[(row, col) for col in range(len(data[row])) if data[row][col]=="S"] for row in range(len(data))]
 	start = tuple(chain(*start))[0]
-	steps = 0
-	end_walk = False
-	visited = set()
+	steps = [0, 0]
+	start_walk = False
+	visited = set([start])
 	positions = [start, start]
-	while not end_walk:
+	while True:
 		for id, position in enumerate(positions):
-			positions[id] = scan_neighbors(data, position)
-			if positions[id] not in visited:
-				visited.add(positions[id])
-			else:
-				pass
-		steps += 1
-		if positions[0] == positions[1]:
-			break
-		
+			#Janky way to avoid first case where start/end match
+			if positions[0] == positions[1] and not start_walk:
+				start_walk = True
+				end_walk = False
+			
+			for direction, (x, y) in DIRS_DICT.items():
+				next_p = (position[0] + x, position[1] + y)
+				if next_p not in visited:
+					next_step = scan_neighbors(data, position, next_p, direction)
+					if next_step:
+						positions[id] = next_step
+						visited.add(next_step)
+						steps[id] += 1
+						break #Need this break here to get out of the directional search. 
 
-	return steps
+			if positions[0] == positions[1]:
+				start_walk == False
+				end_walk = True
+
+		#To print the table after every 2 moves below
+		sp = None
+		for row in range(len(data)):
+			for col in range(len(data[1])):
+				if (row, col) == positions[0] or (row, col) == positions[1]:
+					sp = "\033[1m" + data[row][col] + "\033[0m"
+					col_id = col
+			if sp:
+				if col_id == 0:
+					print(f"[*{sp}*, {str(data[row][col_id+1:])[1:]}")
+				elif col_id == 4:
+					print(f"{str(data[row][:col_id])[:-1]}, *{sp}*,")
+				else:
+					print(f"{str(data[row][:col_id])[:-1]}, *{sp}*, {str(data[row][col_id+1:])[1:]}")
+				
+				sp = None
+			else:
+				print(data[row][:])
+
+		print("-"*30)
+		# if positions[0] == positions[1]:
+		# 	print("WINNA WINNA CHICKEN DINNA")
+		# 	end_walk = True
+
+	if steps[0] == steps[1]:
+		return steps[0]
 	
 	#Intial path search, 
 	#Then create two positions to track?
@@ -80,7 +120,7 @@ def part_B():
 	data = data_load("test_data")
 	
 print(f"Part A solution: \n{part_A()}\n")
-print(f"Part B solution: \n{part_B()}\n")
+# print(f"Part B solution: \n{part_B()}\n")
 print(f"Lines of code \n{recurse_dir(DAY)}")
 
 ########################################################
